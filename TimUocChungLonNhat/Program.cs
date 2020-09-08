79cdebea941dddf986efaab8d3bfdbf4bb35527a
trinhnam12345z <trinhnam123456789z@gmail.com>
2020-09-08 16:39:09 +0700

    Tìm ước số chung lơn nhất

---
+++ TimUocChungLonNhat/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TimUocChungLonNhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n1, n2, j, uscln = 1;
+            Console.Write("\n");
+            Console.Write("CT Tim uoc so chung lon nhat trong :\n");
+            Console.Write("-----------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Nhap so thu nhat: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap so thu hai: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            j = (n1 < n2) ? n1 : n2;
+            for (i = 1; i <= j; i++)
+            {
+                if (n1 % i == 0 && n2 % i == 0)
+                {
+                    uscln = i;
+                }
+            }
+            Console.Write("\nUSCLN cua {0} va {1} la: {2}\n\n", n1, n2, uscln);
+
+            Console.ReadKey();
+        }
+    }
+}
